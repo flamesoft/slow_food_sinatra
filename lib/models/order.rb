3f039362a904e579b5390a9a4c04c9cc42df849d
Yan Zhang <emma2740@gmail.com>
2016-05-16 09:08:12 +0200

    Add total_price to order

--- lib/models/order.rb
+++ lib/models/order.rb
@@ -4,7 +4,7 @@ class Order
   property :id, Serial, key: true
   property :user_id, String, length: 128
   property :delivery_time, String, length: 128
-
+  property :total_price, Integer
   has n, :order_items, :child_key => [ 'id' ]
 
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -2,6 +2,7 @@ describe Order do
   it { is_expected.to have_property :id }
   it { is_expected.to have_property :user_id }
   it { is_expected.to have_property :delivery_time }
+  it { is_expected.to have_property :total_price }
 #  it { is_expected.to have_property(:order_items) }
 
 end
