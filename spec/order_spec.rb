ef0df60a28a9acb05ecbbfa11b52635c9fa2f424
Yan Zhang <emma2740@gmail.com>
2016-05-19 11:18:04 +0200

    Add test matcher for one-many relationship

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -3,6 +3,6 @@ describe Order do
   it { is_expected.to have_property :user_id }
   it { is_expected.to have_property :delivery_time }
   it { is_expected.to have_property :total_price }
-#  it { is_expected.to have_property(:order_items) }
+  it { is_expected.to have_many :order_items }
 
 end
